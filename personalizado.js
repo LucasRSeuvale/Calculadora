d29b087611c5d743f24ea4c8959d99c274eeb870
Lucas Rodrigues <lucas.rodrigues@seuvale.com>
2022-06-30 21:21:30 -0300

    30-06

---
+++ personalizado.js
@@ -0,0 +1,6 @@
+const previousOperation = document.queryselector("#previous-operation")
+const currentOperationtext = document.queryselector("#current-operations")
+const buttons = document.querySelectorAll("#buttons-container button")
+
+
+console.log("buttons");
